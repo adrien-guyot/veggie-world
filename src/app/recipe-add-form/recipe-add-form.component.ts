120489ed5df826ae999df117903ab2b7ef9d8ff2
mookylicious <adrienguyot17@gmail.com>
2017-10-12 14:39:33 +0200

    Création du formulaire spécifique à l'ajout d'ingrédients et modification de la base de données ingredients.json

--- src/app/recipe-add-form/recipe-add-form.component.ts
+++ src/app/recipe-add-form/recipe-add-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';    // c'est grâce au formBuilder que l'on créé les formGroup
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';    // c'est grâce au formBuilder que l'on créé les formGroup
 import { FormRecipePipe } from './../pipes/form-recipe.pipe';
 
 @Component({
@@ -10,56 +10,103 @@ import { FormRecipePipe } from './../pipes/form-recipe.pipe';
 export class RecipeAddFormComponent implements OnInit {
 
   /*************** REACTIVE FORM ***************/
-  form: FormGroup;
+  recipeForm: FormGroup;
 
   ingredientsForm: FormGroup;
 
   ingredientsList = [];
 
   mealTypes = [
-    {id: 1, name: 'Entrée', value: 'starter'},
-    {id: 2, name: 'Plat', value: 'main-course'},
-    {id: 3, name: 'Dessert', value: 'dessert'},
-    {id: 4, name: 'Fromage', value: 'cheese'}
+    { id: 1, name: 'Entrée', value: 'starter' },
+    { id: 2, name: 'Plat', value: 'main-course' },
+    { id: 3, name: 'Dessert', value: 'dessert' },
+    { id: 4, name: 'Fromage', value: 'cheese' }
   ];
 
   diffLevels = [
-    {id: 1, name: 'Facile', value: 'easy'},
-    {id: 2, name: 'Moyen', value: 'medium'},
-    {id: 3, name: 'Difficile', value: 'hard'},
-    {id: 3, name: 'Expert', value: 'chief'}
+    { id: 1, name: 'Facile', value: 'easy' },
+    { id: 2, name: 'Moyen', value: 'medium' },
+    { id: 3, name: 'Difficile', value: 'hard' },
+    { id: 3, name: 'Expert', value: 'chief' }
+  ];
+
+  ingOptions = [
+    { id: 1, name: 'coulis' },
+    { id: 2, name: 'jus ' },
+    { id: 3, name: 'pulpe' },
+    { id: 4, name: 'purée' }
+  ];
+
+  ingSpec = [
+    { id: 1, name: '3 couleurs' },
+    { id: 2, name: 'au blé dur' },
+    { id: 3, name: 'blanc(s)' },
+    { id: 4, name: 'blanche(s)' },
+    { id: 5, name: 'complet(s)' },
+    { id: 6, name: 'complète(s)' },
+    { id: 7, name: 'confit(es)' },
+    { id: 8, name: 'demi complet(s)' },
+    { id: 9, name: 'demi complète(s)' },
+    { id: 10, name: 'deshydraté(es)' },
+    { id: 11, name: 'pelé(es)' },
+    { id: 12, name: 'rapé(es)' },
+    { id: 13, name: 'seché(es)' }
+  ];
+
+  unitsOfMeasure = [
+    { id: 1, name: 'milligramme(s)', value: 'mg' },
+    { id: 2, name: 'gramme(s)', value: 'g' },
+    { id: 3, name: 'kilogramme(s)', value: 'kg' },
+    { id: 4, name: 'tasse(s)', value: 'tasse(s)' },
+    { id: 5, name: 'millilitre(s)', value: 'ml' },
+    { id: 6, name: 'centilitre(s)', value: 'cl' },
+    { id: 7, name: 'décilitre(s)', value: 'dl' },
+    { id: 8, name: 'litre(s)', value: 'l' },
+    { id: 9, name: 'cuillère(s) à café', value: 'cuillère(s) à café' },
+    { id: 10, name: 'cuillère(s) à soupe', value: 'cuillère(s) à soupe' }
   ];
 
   constructor(private formBuilder: FormBuilder) { }  // Injection du formBuilder
 
+
   ngOnInit() {
-    this.form = this.formBuilder.group({             // Récupération de ce que créé le formBuilder à l'aide de sa méthode group
+    this.createForm();
+  }
+
+  createForm() {
+    this.recipeForm = this.formBuilder.group({  // <-- Parent FormGroup : Récup de ce que créé le formBuilder ac sa méthode group
       id: -1,
-      title: '',
-      publishdate: new Date(),
+      title: ['', Validators.required],
+      publishDate: new Date(),
       mealType: '',
       diffLevel: '',
-      prepdurationhour: '',
-      prepdurationmin: '',
-      cookdurationhour: '',
-      cookdurationmin: '',
-      standindTimehour: '',
-      standindTimenmin: '',
+      prepDurationHour: '',
+      prepDurationMin: '',
+      cookDurationHour: '',
+      cookDurationMin: '',
+      standingTimeHour: '',
+      standingTimeMin: '',
       servings: '',
       author: '',
-      ingredients: '',
+      ingredient: this.formBuilder.group({      // <-- Child FormGroup
+        quantity: '',
+        unit: '',
+        option: '',
+        name: '',
+        spec: '',
+      }),
       directions: '',
     });
   }
 
-  createRecipe(){
-    console.log(this.form.value);
+  createRecipe() {
+    console.log(this.recipeForm.value);
   }
 
-  // onAddProperty() {
-  //   for(let i = 1; i <= 4; i++) {
-  //     (<FormArray>this.ingredientsForm.get('properties')).push(new FormControl());
-  //   }
-  // }
+  addIngredientToIngredientsList() {
+    // for(let i = 1; i <= 4; i++) {
+    //   (<FormArray>this.ingredientsForm.get('properties')).push(new FormControl());
+    // }
+  }
 
 }
